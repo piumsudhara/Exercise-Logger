957f72666a3736c9c4c9c5d0a04ede1b36364331
Pium Sudhara Karunasena <pium.karunasena@gmail.com>
2020-08-02 01:09:46 +0530

    First Commit

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditExercises extends Component{
+    render() {
+        return (
+            <div>
+                <p>You Are On The Edit Exercises Component</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ExercisesList extends Component{
+    render() {
+        return (
+            <div>
+                <p>You Are On The Exercises List Component</p>
+            </div>
+        )
+    }
+}
